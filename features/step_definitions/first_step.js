26a3a10bb5ec51029ebe1d699d0c17ddb6bfe1d5
Dhivya <dhivyada@thoughtworks.com>
2014-08-09 13:12:18 +0530

    [Deepak/DD] just playing around

--- features/step_definitions/first_step.js
+++ features/step_definitions/first_step.js
@@ -1,17 +1,29 @@
-var first_step = function (){
-  this.World = require("../support/world.js").World; 
+var first_step = function () {
+    this.World = require("../support/world.js").World;
 
-this.Given(/^this step is running$/, function (callback) {
-  callback();
-});
+    this.Given(/^this step is running$/, function (callback) {
+        var driver = this.browser;
+        var by = this.wd.By;
+        driver.get("http://www.google.com");
+        driver.findElement(by.name("q")).sendKeys("webdriver");
+        driver.findElement(by.name("btnG")).click();
+        var promise = driver.getTitle();
+        promise.then(function (title) {
+            console.log("got the title");
+            assertEquals("webdriver - Google Search", title);
+            driver.quit();
+        });
 
-this.When(/^this step also runs$/, function (callback) {
-  callback();
-});
+        callback();
+    });
 
-this.Then(/^the test passes$/, function (callback) {
-  callback();
-});
+    this.When(/^this step also runs$/, function (callback) {
+        callback();
+    });
+
+    this.Then(/^the test passes$/, function (callback) {
+        callback();
+    });
 
 };
 
--- features/support/world.js
+++ features/support/world.js
@@ -1,4 +1,16 @@
+
 var World = function World(callback) {
-   callback(); 
+    this.wd = require('selenium-webdriver');
+    this.browser = new this.wd.Builder().withCapabilities(this.wd.Capabilities.chrome()).build();
+
+    this.visit = function(url) {
+        this.browser.get(url);
+    };
+
+    this.quit = function(){
+        this.browser.quit();
+    };
+
+    callback(); // tell Cucumber we're finished and to use 'this' as the world instance
 };
 exports.World = World;
